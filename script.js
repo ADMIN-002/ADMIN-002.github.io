6bfeee5849604cb2c72260ff1a830560d773a1fc
ADMIN-002 <mmcyt454@gmail.com>
2024-11-05 09:59:04 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example: Smooth scroll to sections
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
